3fb10fb5f0e2f13f4b4289f12b95d36607e505d7
hemilynara <hemilynara@gmail.com>
2024-05-18 13:34:34 -0300

    first commit

--- obj/Debug/net8.0/ChurnAnalytics.AssemblyInfo.cs
+++ obj/Debug/net8.0/ChurnAnalytics.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ChurnAnalitycs")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ChurnAnalytics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5d15ce89ed569672c0e34f35f76a59de6f52947")]
-[assembly: System.Reflection.AssemblyProductAttribute("ChurnAnalitycs")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ChurnAnalitycs")]
+[assembly: System.Reflection.AssemblyProductAttribute("ChurnAnalytics")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ChurnAnalytics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
