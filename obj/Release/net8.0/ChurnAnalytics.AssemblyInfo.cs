684038157f47ba1452bf395cfaaa76e1f6e6eda4
hemilynara <hemilynara@gmail.com>
2024-05-18 13:35:19 -0300

    first commit

--- obj/Release/net8.0/ChurnAnalytics.AssemblyInfo.cs
+++ obj/Release/net8.0/ChurnAnalytics.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChurnAnalytics")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8092897001eafc24a660d067cdfa9726c1889ee1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChurnAnalytics")]
